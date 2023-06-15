6406e8050df41156515551210e8cc9facdcb627e
AlanFerreira <alanjjr83@gmail.com>
2023-06-14 23:03:00 -0300

    reafactor: ajustar botoes e redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,44 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import {
+  decrement,
+  decrementAmount,
+  increment,
+  incrementAmount,
+} from './features/counter/counter-slice'
+import { useAppSelector } from './hooks'
+import { Loading } from './components'
 
 // OLD SCHOOL REDUX
 // function App({ count, increment, incrementAmount}) {
 function App() {
-  const count = useSelector((state) => state.counter.value)
+  // OLD SCHOOL REDUX
+  // const count = useSelector((state) => state.counter.value)
+  const count = useAppSelector((state) => state.counter.value)
   const dispatch = useDispatch()
 
   function handleOnClick() {
     // OLD SCHOOL REDUX
     // increment()
-    dispatch({ type: 'counter/increment' })
+    // dispatch({ type: 'counter/increment' })
+    dispatch(increment())
   }
 
   function handleOnClickAmount() {
     // OLD SCHOOL REDUX
     // incrementAmount(5)
-    dispatch({ type: 'counter/incrementAmount', payload: 5 })
+    // dispatch({ type: 'counter/incrementAmount', payload: 5 })
+    dispatch(incrementAmount(5))
+  }
+
+  function handleClickDecrement() {
+    dispatch(decrement())
+  }
+
+  function handleClickDecrementAmount() {
+    dispatch(decrementAmount(5))
   }
 
   return (
@@ -35,11 +55,23 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={handleOnClick}>count is {count}</button>
-        <button onClick={handleOnClickAmount}>Increment Amount</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h1 className="card-item-1">{count}</h1>
+        <div className="card-item-2">
+          <button className="card-button" onClick={handleOnClick}>
+            Increment to one in one
+          </button>
+          <button className="card-button" onClick={handleOnClickAmount}>
+            Increment Amount
+          </button>
+        </div>
+        <div className="card-item-3">
+          <button className="card-button" onClick={handleClickDecrement}>
+            Decrement to one in one
+          </button>
+          <button className="card-button" onClick={handleClickDecrementAmount}>
+            Decrement Amount
+          </button>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
